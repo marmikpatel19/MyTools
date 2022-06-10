7f69d8eac9c63f9ae87fe66834386abe4b6e7ba0
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-10 08:56:12 -0600

    add routes

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
 
+// Tools
+const tools = [
+  { id: 7, name: "trowel", cost: 4.3 },
+  { id: 3, name: "shovel", cost: 45.0 },
+  { id: 4, name: "wheelbarrow", cost: 60.0 },
+  { id: 2, name: "tree-spade", cost: 34999.99 },
+  { id: 11, name: "gloves", cost: 4.99 },
+];
+
 // Port
 const port = process.env.PORT || 3000;
+app.listen(port, () => {});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(`Visit http:/localhost:${port}/api/tools to see a list of tools`);
+});
+
+app.get("/api/tools", (req, res) => {
+  res.send(tools);
+});
